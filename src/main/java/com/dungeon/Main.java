2963cad5948471dfcc2eac9f1b98e8d6fa89108e
npaccoud <nathpacc@gmail.com>
2026-03-31 11:06:00 +0200

    fix: Generating a completable dungeon

--- src/main/java/com/dungeon/Main.java
+++ src/main/java/com/dungeon/Main.java
@@ -4,16 +4,25 @@ import com.dungeon.model.*;
 import com.dungeon.system.GameWindow;
 
 public class Main {
-       public static void main(String[] args) {
-        
+    public static void main(String[] args) {
+        // 1. Initialisation
         Dungeon dungeon = new Dungeon(15, 15);
         Hero hero = new Hero("Lancelot", 100, 15);
-        Enemy enemy = new Enemy();
+        hero.setPosition(new Coordinates(1, 0));
+        
+        // 2. Génération du donjon (le donjon s'assure d'être valide par rapport au héros)
+        dungeon.generate(hero.getPosition());
 
+        // 3. Placement de l'ennemi sur une case vide aléatoire
+        Enemy enemy = new Enemy();
+        Coordinates enemySpawn = dungeon.findRandomEmptyPos();
+        enemy.setPosition(enemySpawn);
+
+        // 4. Lancement de l'interface
         javax.swing.SwingUtilities.invokeLater(() -> {
             new GameWindow(dungeon, hero, enemy);
         });
 
-        System.out.println("Jeu lancé ! Utilisez ZQSD pour vous déplacer.");
+        System.out.println("Donjon généré et validé !");
     }
 }
--- src/main/java/com/dungeon/model/Dungeon.java
+++ src/main/java/com/dungeon/model/Dungeon.java
@@ -6,67 +6,97 @@ public class Dungeon {
     private int width;
     private int height;
     private Cell[][] grid;
+    private Coordinates stairPos;
+    private final Random rand = new Random();
 
     public Dungeon(int width, int height) {
         this.width = width;
         this.height = height;
         this.grid = new Cell[width][height];
-        
-        generate();
+        // On ne génère rien ici, on laisse le Main décider quand lancer la génération
     }
 
-    private void generate() {
-        Random rand = new Random();
-        // Generating grid with entry, walls around the room and random cells elswhere
+    public void generate(Coordinates heroPos) {
+        boolean dungeonReady = false;
+        int tries = 0;
+
+        while (!dungeonReady) {
+            // 1. Remplissage de base
+            fillWithRandomTiles();
+            
+            // 2. Placement de l'escalier sur une case FLOOR
+            this.stairPos = findRandomEmptyPos();
+            grid[stairPos.getX()][stairPos.getY()].setType("STAIRS");
+
+            // 3. Test de connectivité (Héros -> Escalier)
+            // On ne teste pas l'ennemi ici, on le placera sur une case déjà validée après !
+            if (isReachable(heroPos, stairPos)) {
+                dungeonReady = true;
+            }
+            tries++;
+            if (tries%1000 == 0) {
+                System.err.println("Milles tentatives passées");
+            }
+        }
+    }
+
+    private void fillWithRandomTiles() {
         for (int i = 0; i < width; i++) {
             for (int j = 0; j < height; j++) {
-                if (i == 0 || i == width-1 || j == 0 || j == height-1) {
+                // 1. On définit d'abord les murs de bordure
+                if (i == 0 || i == width - 1 || j == 0 || j == height - 1) {
                     grid[i][j] = new Cell("WALL");
-                } else {
-                    if (rand.nextInt(3) == 0) {
-                        grid[i][j] = new Cell("WALL");
-                    } else {
-                        grid[i][j] = new Cell("FLOOR");
-                    }
+                } 
+                // 2. On définit le reste aléatoirement
+                else {
+                    grid[i][j] = new Cell(rand.nextInt(3) == 0 ? "WALL" : "FLOOR");
                 }
             }
         }
-        // Placing stair
-        int randomX = rand.nextInt(width - 2) + 1;
-        int randomY = rand.nextInt(height - 2) + 1;
-        grid[randomX][randomY].setType("STAIRS");
-        grid[1][0].setType("FLOOR");
+        // 3. À LA TOUTE FIN, on force le passage pour le héros
+        // On utilise (1, 1) car (1, 0) est dans le mur de bordure !
+        grid[1][0].setType("FLOOR"); 
+    }
+
+    // Trouve une case FLOOR qui n'est pas l'entrée
+    public Coordinates findRandomEmptyPos() {
+        int rx, ry;
+        do {
+            rx = rand.nextInt(width - 2) + 1;
+            ry = rand.nextInt(height - 2) + 1;
+        } while (!grid[rx][ry].getType().equals("FLOOR"));
+        return new Coordinates(rx, ry);
+    }
+
+    private boolean isReachable(Coordinates start, Coordinates end) {
+        boolean[][] visited = new boolean[width][height];
+        floodFill(start.getX(), start.getY(), visited);
+        return visited[end.getX()][end.getY()];
+    }
+
+    private void floodFill(int x, int y, boolean[][] visited) {
+        if (isOutOfBounds(x, y) || visited[x][y] || grid[x][y].getType().equals("WALL")) {
+            return;
+        }
+        visited[x][y] = true;
+        floodFill(x + 1, y, visited);
+        floodFill(x - 1, y, visited);
+        floodFill(x, y + 1, visited);
+        floodFill(x, y - 1, visited);
+    }
+
+    public boolean isWalkable(Coordinates pos) {
+        if (isOutOfBounds(pos.getX(), pos.getY())) return false;
+        return !grid[pos.getX()][pos.getY()].getType().equals("WALL");
     }
 
     private boolean isOutOfBounds(int x, int y) {
-    return x < 0 || x >= this.width || y < 0 || y >= this.height;
-}
-
-    public boolean isWalkable(Coordinates nextPos) {
-        int x = nextPos.getX();
-        int y = nextPos.getY();
-
-        if (isOutOfBounds(x,y)) {
-            return false;
-        }
-
-        Cell targetedCell=grid[x][y];
-        if (targetedCell.getType().equals("WALL")) {
-            return false;
-        }
-        
-        return true; 
+        return x < 0 || x >= width || y < 0 || y >= height;
     }
 
-    public int getWidth() {
-        return this.width;
-    }
-
-    public int getHeight() {
-        return this.height;
-    }
-
-    public Cell getCell(int x, int y){
-        return this.grid[x][y];
-    }
+    // Getters
+    public Cell getCell(int x, int y) { return grid[x][y]; }
+    public int getWidth() { return width; }
+    public int getHeight() { return height; }
+    public Coordinates getStairPos() { return stairPos; }
 }
